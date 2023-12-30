4d944b16dd500ba0e032bb962c227f789290dc37
Aki Moto <denyuking.volval8@gmail.jp>
2023-12-30 12:23:45 +0900

    add

--- ros2_ws/src/mypkg/setup.py
+++ ros2_ws/src/mypkg/setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
-import os
-from glob import glob
 
 package_name = 'mypkg'
 
@@ -12,7 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
